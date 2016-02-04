aaea50160ae46224c4421ca743aa20b0f1b4da2c
Brena Elric <Brena Elric>
2016-02-04 18:32:08 -0300

    primeiro

---
+++ rec1.c
@@ -0,0 +1,14 @@
+int rec1 (int n)
+{
+  if (n==1)
+    return 1;
+  else
+    return rec1 + 1;
+}
+
+int int main()
+{
+  int n;
+  printf("Digite um número natural: \n", );
+  scanf("%d\n", &n);
+  printf("%dº\n", );
